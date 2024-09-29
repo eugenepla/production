f39c4c3f2468d6e54dea6fb0d08ac7441242aa52
Eugene Plakhotnikov <eugene.plakhotnikov@miltech.dev>
2024-09-29 08:48:23 +0300

    decomposition webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,9 +4,9 @@ import { BuildPaths } from "./config/build/types/config"
 import path from "path"
 
 const paths: BuildPaths = {
-  entry: path.resolve(__dirname, "public", "index.html"),
+  entry: path.resolve(__dirname, "src", "index.ts"),
   build: path.resolve(__dirname, "build"),
-  html: path.resolve(__dirname, "src", "index.ts"),
+  html: path.resolve(__dirname, "public", "index.html"),
 }
 
 const mode = "development"
