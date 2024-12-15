80a9ada6b3474eb6a6414f9873441ec430c2fbfa
Eugene Plakhotnikov <eugene.plakhotnikov@miltech.dev>
2024-12-15 11:24:21 +0200

    add react testing library, try tests for sidebar button

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,32 +1,32 @@
-import { classNames } from "shared/lib/classNames/classNames"
+import { classNames } from 'shared/lib/classNames/classNames';
 
-describe("classNames", () => {
-  test("with only first param", () => {
-    expect(classNames("someClass")).toBe("someClass")
-  })
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
 
-  test("with addittional class", () => {
-    const expected = "someClass class1 class2"
-    expect(classNames("someClass", {}, ["class1", "class2"])).toBe(expected)
-  })
+    test('with addittional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+    });
 
-  test("with mods ", () => {
-    const expected = "someClass class1 class2 hovered scrollable"
-    expect(
-      classNames("someClass", { hovered: true, scrollable: true }, [
-        "class1",
-        "class2",
-      ])
-    ).toBe(expected)
-  })
+    test('with mods ', () => {
+        const expected = 'someClass class1 class2 hovered scrollable';
+        expect(
+            classNames('someClass', { hovered: true, scrollable: true }, [
+                'class1',
+                'class2',
+            ]),
+        ).toBe(expected);
+    });
 
-  test("with mods ", () => {
-    const expected = "someClass class1 class2 hovered"
-    expect(
-      classNames("someClass", { hovered: true, scrollable: undefined }, [
-        "class1",
-        "class2",
-      ])
-    ).toBe(expected)
-  })
-})
+    test('with mods ', () => {
+        const expected = 'someClass class1 class2 hovered';
+        expect(
+            classNames('someClass', { hovered: true, scrollable: undefined }, [
+                'class1',
+                'class2',
+            ]),
+        ).toBe(expected);
+    });
+});
